eb5bcd074af9757a39f482ee38443a5e1636a3b7
sarrside <side.sarr@uadb.edu.sn>
2019-04-15 19:55:45 +0000

    Mis en place de tous les methodes

--- src/model/ActeurModel.java
+++ src/model/ActeurModel.java
@@ -5,44 +5,125 @@ package model;
  * Purpose: Defines the Class ActeurModel
  ***********************************************************************/
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 import shared.Acteur;
+import shared.Cinema;
 
 /** @pdOid f9bd4a78-e1e2-453c-923c-68eab2d069ae */
 public class ActeurModel {
+	
+	static Connection con=new ConnexionDB().connexionBD();
+	static Statement statement=null;
    /** @param acteur
     * @pdOid 1ae608ad-64e1-4a0c-833d-4f3f79ff9dc6 */
    public static boolean addActeur(Acteur acteur) {
-      // TODO: implement
-      return false;
+	   String sql="INSERT into Acteur () VALUES ("+acteur.getNom()+","+acteur.getPrenom()+","+acteur.getNationalite()+","+acteur.getDnaiss()+","+acteur.getLnaiss()+")";
+	   boolean res=false;
+	   try {
+		statement=con.createStatement();
+		res=statement.execute(sql);
+		con.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+      
+      return res;
    }
    
    /** @param acteur
     * @pdOid 0fdbc329-291f-4048-9a42-2fbef0cb0a33 */
    public static boolean modifyActeur(Acteur acteur) {
-      // TODO: implement
-      return false;
+	   String sql="UPDATE Acteur set nom=?,prenom=?,dateNaissance=?,lieuNaissance=?,nationalite=? where idActeur=?";
+	   boolean res=false;
+	   try {
+		PreparedStatement pstatement=con.prepareStatement(sql);
+		pstatement.setString(1, acteur.getNom());
+		pstatement.setString(2, acteur.getPrenom());
+		pstatement.setDate(3, (java.sql.Date)acteur.getDnaiss());
+		pstatement.setString(4, acteur.getLnaiss());
+		pstatement.setString(5, acteur.getNationalite());
+		pstatement.setInt(6,acteur.idPers);
+		res=pstatement.execute();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+      return res;
    }
    
    /** @param acteur
     * @pdOid f489ed3c-791d-4ffa-95a8-c3225cab61a7 */
    public static boolean deleteActeur(Acteur acteur) {
-      // TODO: implement
-      return false;
+      String sql="Delete from Acteur where idActeur=?";
+      boolean res=false;
+      try {
+		PreparedStatement pstatement=con.prepareStatement(sql);
+		pstatement.setInt(1, acteur.getIdPers());
+		res=pstatement.execute();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+      return res;
    }
    
    /** @pdOid 900c70cf-0cc3-4539-9afc-4efa35a983c5 */
-   public static Acteur getActeurs() {
-      // TODO: implement
+   public static List<Acteur> getActeurs() {
+	   List<Acteur> liste=null;
+	   String sql="SELECT * FROM Acteur";
+	   boolean res=false;
+	   try {
+		   liste=new ArrayList<Acteur>();
+		PreparedStatement pstatement =con.prepareStatement(sql);
+		ResultSet resultat=pstatement.executeQuery();
+		while(resultat.next()) {
+			Acteur acteur=new Acteur();
+			acteur.setIdPers(resultat.getInt("adresse"));
+			acteur.setDnaiss(resultat.getDate("dnaiss"));
+			acteur.setLnaiss(resultat.getString("lnaiss"));
+			acteur.setNationalite(resultat.getString("nationalite"));
+			acteur.setNom(resultat.getString("nom"));
+			acteur.setPrenom(resultat.getString("prenom"));
+			liste.add(acteur);
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
       return null;
    }
    
    /** @param idActeur
     * @pdOid 7a448f41-198a-46a1-ae2c-afef46ab76f7 */
    public static Acteur getActeur(int idActeur) {
-      // TODO: implement
-      return null;
+	   String sql="SELECT * FROM acteur where idCinema=?";
+	   PreparedStatement pstatement;
+	   Acteur acteur=new Acteur();
+	try {
+		pstatement = con.prepareStatement(sql);
+		pstatement.setInt(1, idActeur);
+		
+		ResultSet resultat=pstatement.executeQuery();
+		while(resultat.next()) {
+			acteur.setIdPers(resultat.getInt("adresse"));
+			acteur.setDnaiss(resultat.getDate("dnaiss"));
+			acteur.setLnaiss(resultat.getString("lnaiss"));
+			acteur.setNationalite(resultat.getString("nationalite"));
+			acteur.setNom(resultat.getString("nom"));
+			acteur.setPrenom(resultat.getString("prenom"));
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+      return acteur;
    }
 
 }
--- src/shared/Acteur.java
+++ src/shared/Acteur.java
@@ -5,21 +5,75 @@ package shared;
  * Purpose: Defines the Class Acteur
  ***********************************************************************/
 
+import java.io.Serializable;
 import java.util.*;
 
 /** @pdOid cd6998cc-8e02-4654-b0ee-ba3c55ce7a17 */
-public class Acteur {
-   /** @pdOid 15283efb-99fb-42fc-97ed-3954d3b9d882 */
+public class Acteur implements Serializable{
    public int idPers;
-   /** @pdOid fb4afe38-fca9-4dae-b1bd-a8b45b3d7400 */
    public String nom;
-   /** @pdOid 9564dec0-e771-4c0f-8dd9-2a01a59ca945 */
    public String prenom;
-   /** @pdOid dd8cb5c7-f3a4-4436-86e5-9724cd44dc24 */
    public String nationalite;
-   /** @pdOid 59b92bfd-13f3-4413-9294-835fdbc5c6e4 */
    public java.util.Date dnaiss;
-   /** @pdOid fed8beb7-9290-46c1-b465-fcab4e0ff2af */
    public String lnaiss;
+   
+   
+   
+   
+public Acteur() {
+}
+
+
+public Acteur(int idPers, String nom, String prenom, String nationalite, Date dnaiss, String lnaiss) {
+	super();
+	this.idPers = idPers;
+	this.nom = nom;
+	this.prenom = prenom;
+	this.nationalite = nationalite;
+	this.dnaiss = dnaiss;
+	this.lnaiss = lnaiss;
+}
+
+
+public int getIdPers() {
+	return idPers;
+}
+public void setIdPers(int idPers) {
+	this.idPers = idPers;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public String getNationalite() {
+	return nationalite;
+}
+public void setNationalite(String nationalite) {
+	this.nationalite = nationalite;
+}
+public java.util.Date getDnaiss() {
+	return dnaiss;
+}
+public void setDnaiss(java.util.Date dnaiss) {
+	this.dnaiss = dnaiss;
+}
+public String getLnaiss() {
+	return lnaiss;
+}
+public void setLnaiss(String lnaiss) {
+	this.lnaiss = lnaiss;
+}
+   
+   
+   
+   
 
 }
